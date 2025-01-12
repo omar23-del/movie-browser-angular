a3b38cd420101cf0f8f8d46bd4e9efc20987bcca
omar-12 <ok2903474@gmail.com>
2025-01-12 15:07:33 +0200

    update

--- src/app/movies-service.service.ts
+++ src/app/movies-service.service.ts
@@ -1,14 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { Movies } from './interfaces/movies';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MoviesServiceService {
-private watchlist = new BehaviorSubject<Movies[]>([])
-watchlist$ = this.watchlist.asObservable()
+
 
 
   constructor(private httpclient: HttpClient) {}
@@ -39,10 +37,4 @@ watchlist$ = this.watchlist.asObservable()
       `https://api.themoviedb.org/3/tv/${id}?api_key=ac4ca2fdf78da1a857a35e132fda928b&language=en-US&page=1`);
   }
 
-  addtowatchlist(movies: Movies) {
-    const currentlist = this.watchlist.value
-    if(!currentlist.find(m => m.id === movies.id)) {
-      this.watchlist.next([...currentlist, movies])
-    }
-  }
 }
